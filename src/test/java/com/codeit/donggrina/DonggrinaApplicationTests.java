72893604f4ab49bc95b98809ab6175fff88e4356
hyunzxn <jann1653@gmail.com>
2024-05-27 15:53:19 +0900

    fix: ci/cd 오류 해결 위해서 test 코드 쪽 수정

--- src/test/java/com/codeit/donggrina/DonggrinaApplicationTests.java
+++ src/test/java/com/codeit/donggrina/DonggrinaApplicationTests.java
@@ -3,7 +3,7 @@ package com.codeit.donggrina;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = DonggrinaApplication.class)
 class DonggrinaApplicationTests {
 
     @Test
