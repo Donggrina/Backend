fdce54a690f1bb2a14e5afb287026ff79fe351e4
kwonMInwoo <qwerty029369@naver.com>
2024-05-28 15:50:14 +0900

    feat: ProfileImage 추가
    - 사용자 프로필 조회 시 프로필 이미지도 함께 조회해야 하므로 ProfileImage 엔티티 추가

--- src/main/java/com/codeit/donggrina/domain/member/entity/Member.java
+++ src/main/java/com/codeit/donggrina/domain/member/entity/Member.java
@@ -1,10 +1,13 @@
 package com.codeit.donggrina.domain.member.entity;
 
+import com.codeit.donggrina.domain.ProfileImage.entity.ProfileImage;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
 import jakarta.persistence.NamedEntityGraph;
+import jakarta.persistence.OneToOne;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
@@ -20,12 +23,16 @@ public class Member {
     private String username;
     private String name;
     private String role;
+    @OneToOne
+    @JoinColumn(name = "profile_image_id")
+    private ProfileImage profileImage;
 
     @Builder
-    private Member(Long id, String username, String name, String role) {
+    private Member(Long id, String username, String name, String role, ProfileImage profileImage) {
         this.id = id;
         this.username = username;
         this.name = name;
         this.role = role;
+        this.profileImage = profileImage;
     }
 }
