43c5e2a8bfa503b4b1193e8e8fc06b40e46a6f20
kwonMInwoo <qwerty029369@naver.com>
2024-06-03 13:45:06 +0900

    feat: 사용자 프로필 수정 로직
    - 사용자 프로필을 수정할 수 있는 로직 생성

--- src/main/java/com/codeit/donggrina/domain/member/service/MemberService.java
+++ src/main/java/com/codeit/donggrina/domain/member/service/MemberService.java
@@ -1,16 +1,33 @@
 package com.codeit.donggrina.domain.member.service;
 
+import com.codeit.donggrina.domain.ProfileImage.entity.ProfileImage;
+import com.codeit.donggrina.domain.ProfileImage.repository.ProfileImageRepository;
 import com.codeit.donggrina.domain.member.dto.response.MyProfileGetResponse;
+import com.codeit.donggrina.domain.member.entity.Member;
 import com.codeit.donggrina.domain.member.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
 public class MemberService {
+
     private final MemberRepository memberRepository;
+    private final ProfileImageRepository profileImageRepository;
 
     public MyProfileGetResponse getMyProfile(Long memberId) {
         return memberRepository.findMyProfileById(memberId).orElseThrow(RuntimeException::new);
+    }
+
+    @Transactional
+    public void updateMyProfile(Long memberId, Long imageId, String name) {
+        Member currentMember = memberRepository.findById(memberId)
+            .orElseThrow(RuntimeException::new);
+        ProfileImage updatedProfileImage = profileImageRepository.findById(imageId)
+            .orElseThrow(RuntimeException::new);
+
+        currentMember.updateNickname(name);
+        currentMember.updateProfileImage(updatedProfileImage);
     }
 }
