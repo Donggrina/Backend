667193632ccecde746e46950fba30eea72dcc761
kwonMInwoo <qwerty029369@naver.com>
2024-06-05 21:01:33 +0900

    feat: 다이어리 엔티티 추가
    - 다이어리와 관련된 엔티티 모두 추가

---
+++ src/main/java/com/codeit/donggrina/domain/diary/entity/DiaryPet.java
@@ -0,0 +1,34 @@
+package com.codeit.donggrina.domain.diary.entity;
+
+import com.codeit.donggrina.domain.pet.entity.Pet;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class DiaryPet {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @ManyToOne
+    @JoinColumn(name = "diary_id")
+    private Diary diary;
+    @ManyToOne
+    @JoinColumn(name = "pet_id")
+    private Pet pet;
+
+    @Builder
+    private DiaryPet(Long id, Diary diary, Pet pet) {
+        this.id = id;
+        this.diary = diary;
+        this.pet = pet;
+    }
+}
