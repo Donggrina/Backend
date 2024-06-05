690d6749ae219f5b3cd1e7b32788727c0075c779
kwonMInwoo <qwerty029369@naver.com>
2024-06-05 21:02:35 +0900

    feat: 다이어리 이미지 repository 생성
    - 다이어리에 들어가는 이미지의 repository 생성

---
+++ src/main/java/com/codeit/donggrina/domain/diary/repository/DiaryImageRepository.java
@@ -0,0 +1,8 @@
+package com.codeit.donggrina.domain.diary.repository;
+
+import com.codeit.donggrina.domain.diary.entity.DiaryImage;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface DiaryImageRepository extends JpaRepository<DiaryImage, Long> {
+
+}
