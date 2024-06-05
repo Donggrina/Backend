7073e9ea2589f1893b919053d060afb59995fde3
문현준 <jann1653@gmail.com>
2024-06-05 23:15:19 +0900

    refactor: GroupRepository의 findWithPets 메소드의 리턴 타입을 Group 에서 Optional<Group>으로 변경하여 바로 예외처리

--- src/main/java/com/codeit/donggrina/domain/group/repository/CustomGroupRepository.java
+++ src/main/java/com/codeit/donggrina/domain/group/repository/CustomGroupRepository.java
@@ -2,10 +2,11 @@ package com.codeit.donggrina.domain.group.repository;
 
 import com.codeit.donggrina.domain.group.dto.response.GroupDetailResponse;
 import com.codeit.donggrina.domain.group.entity.Group;
+import java.util.Optional;
 
 public interface CustomGroupRepository {
 
     GroupDetailResponse findGroupDetail(Long groupId);
 
-    Group findWithPets(Long groupId);
+    Optional<Group> findWithPets(Long groupId);
 }
