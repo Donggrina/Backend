8909b39bae6f2a0838c96a7b87c33a0de37cd8ee
kwonMInwoo <qwerty029369@naver.com>
2024-06-07 19:19:31 +0900

    feat: 다이어리 전체 조회 로직
    - 다이어리 전체 조회 시 필요한 데이터를 조회하여 응답하는 로직 작성

--- src/main/java/com/codeit/donggrina/domain/diary/repository/DiaryRepository.java
+++ src/main/java/com/codeit/donggrina/domain/diary/repository/DiaryRepository.java
@@ -1,8 +1,21 @@
 package com.codeit.donggrina.domain.diary.repository;
 
 import com.codeit.donggrina.domain.diary.entity.Diary;
+import com.codeit.donggrina.domain.group.entity.Group;
+import java.time.LocalDate;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface DiaryRepository extends JpaRepository<Diary, Long> {
 
+    @Query("select d from Diary d"
+        + " join fetch d.diaryPets dp"
+        + " join fetch dp.pet p"
+        + " join fetch p.profileImage"
+        + " join fetch d.diaryImages"
+        + " where d.group = :group and d.date = :date")
+    List<Diary> findAllByDate(LocalDate date, Group group);
+
 }
+
