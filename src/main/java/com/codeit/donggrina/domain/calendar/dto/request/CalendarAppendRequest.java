704f3346c2c4b72ac541c41e0abafa63bbc929e8
문현준 <jann1653@gmail.com>
2024-06-08 11:16:02 +0900

    fix: 일정 등록, 수정하는 요청 DTO memo 필드에 유효성 검증 어노테이션 추가

--- src/main/java/com/codeit/donggrina/domain/calendar/dto/request/CalendarAppendRequest.java
+++ src/main/java/com/codeit/donggrina/domain/calendar/dto/request/CalendarAppendRequest.java
@@ -7,6 +7,7 @@ import jakarta.validation.constraints.NotBlank;
 public record CalendarAppendRequest(
     @NotBlank(message = "제목을 입력해주세요.")
     String title,
+    @NotBlank(message = "내용을 입력해주세요.")
     String memo,
     @NotBlank(message = "반려동물을 선택해주세요.")
     String petName,
