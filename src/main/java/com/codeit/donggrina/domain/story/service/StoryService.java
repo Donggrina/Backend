fc17c6764b0dd8a64c24ba25b7688c627b5855f8
kwonMInwoo <qwerty029369@naver.com>
2024-06-13 21:09:17 +0900

    feat: 스토리 삭제 로직 추가
    - 공유 여부를 false로 해 스토리로 조회되지 않게 작성
    - diary와 member를 함께 조회하는 메서드 생성

--- src/main/java/com/codeit/donggrina/domain/story/service/StoryService.java
+++ src/main/java/com/codeit/donggrina/domain/story/service/StoryService.java
@@ -2,6 +2,7 @@ package com.codeit.donggrina.domain.story.service;
 
 import com.codeit.donggrina.domain.diary.entity.Diary;
 import com.codeit.donggrina.domain.diary.repository.DiaryRepository;
+import com.codeit.donggrina.domain.member.dto.request.CustomOAuth2User;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -14,12 +15,25 @@ public class StoryService {
 
     @Transactional
     public void createStory(Long diaryId, Long memberId) {
-        Diary targetDiary = diaryRepository.findById(diaryId).orElseThrow(RuntimeException::new);
+        Diary targetDiary = diaryRepository.findByIdWithMember(diaryId)
+            .orElseThrow(RuntimeException::new);
 
         if(targetDiary.getMember().getId() != memberId) {
             throw new RuntimeException();
         }
 
         targetDiary.shareToStory();
+    }
+
+    @Transactional
+    public void deleteStory(Long diaryId, Long memberId) {
+        Diary targetDiary = diaryRepository.findByIdWithMember(diaryId)
+            .orElseThrow(RuntimeException::new);
+
+        if(targetDiary.getMember().getId() != memberId) {
+            throw new RuntimeException();
+        }
+
+        targetDiary.unShareToStory();
     }
 }
