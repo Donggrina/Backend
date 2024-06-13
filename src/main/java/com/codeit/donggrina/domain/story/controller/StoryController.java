0a1d705901355dfbc33d1c49fbbc26c336a1b20f
kwonMInwoo <qwerty029369@naver.com>
2024-06-13 16:29:59 +0900

    feat: 스토리 생성 로직
    - 공유되지 않은 다이어리를 스토리로 공유하는 로직

---
+++ src/main/java/com/codeit/donggrina/domain/story/controller/StoryController.java
@@ -0,0 +1,31 @@
+package com.codeit.donggrina.domain.story.controller;
+
+import com.codeit.donggrina.common.api.ApiResponse;
+import com.codeit.donggrina.domain.member.dto.request.CustomOAuth2User;
+import com.codeit.donggrina.domain.story.service.StoryService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/stories")
+public class StoryController {
+
+    private final StoryService storyService;
+
+    @PostMapping("/{diaryId}")
+    public ApiResponse<Void> createStory(@PathVariable Long diaryId, @AuthenticationPrincipal
+        CustomOAuth2User user) {
+
+        storyService.createStory(diaryId, user.getMemberId());
+        return ApiResponse.<Void>builder()
+            .code(HttpStatus.CREATED.value())
+            .message("스토리 등록 성공")
+            .build();
+    }
+}
