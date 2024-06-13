c5b7e3768da372eae55b17521b6669ab914a7742
kwonMInwoo <qwerty029369@naver.com>
2024-06-13 14:40:25 +0900

    feat: 다이어리 검색 로직
    - 다이어리 검색 시 필요한 데이터를 조회

--- src/main/java/com/codeit/donggrina/domain/diary/repository/CustomDiaryRepository.java
+++ src/main/java/com/codeit/donggrina/domain/diary/repository/CustomDiaryRepository.java
@@ -1,11 +1,15 @@
 package com.codeit.donggrina.domain.diary.repository;
 
+import com.codeit.donggrina.domain.diary.dto.request.DiarySearchRequest;
 import com.codeit.donggrina.domain.diary.entity.Diary;
+import java.util.List;
 
 public interface CustomDiaryRepository {
 
     void addHeartCount(Diary diary);
 
     void subHeartCount(Diary diary);
+
+    List<Diary> searchDiaries(DiarySearchRequest request);
 
 }
