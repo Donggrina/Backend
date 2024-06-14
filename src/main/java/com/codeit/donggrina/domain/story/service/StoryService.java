727371fa811c18d6797a94e2d50586a783b53b94
kwonMInwoo <qwerty029369@naver.com>
2024-06-14 12:41:11 +0900

    feat: 스토리 상세 조회
    - 스토리 상세 조회 시 필요한 데이터를 반환하는 로직 작성

--- src/main/java/com/codeit/donggrina/domain/story/service/StoryService.java
+++ src/main/java/com/codeit/donggrina/domain/story/service/StoryService.java
@@ -1,8 +1,18 @@
 package com.codeit.donggrina.domain.story.service;
 
+import com.codeit.donggrina.domain.comment.dto.response.CommentFindResponse;
 import com.codeit.donggrina.domain.diary.entity.Diary;
+import com.codeit.donggrina.domain.diary.entity.DiaryImage;
 import com.codeit.donggrina.domain.diary.repository.DiaryRepository;
+import com.codeit.donggrina.domain.heart.entity.Heart;
+import com.codeit.donggrina.domain.heart.repository.HeartRepository;
 import com.codeit.donggrina.domain.member.dto.request.CustomOAuth2User;
+import com.codeit.donggrina.domain.member.entity.Member;
+import com.codeit.donggrina.domain.member.repository.MemberRepository;
+import com.codeit.donggrina.domain.story.dto.response.StoryFindResponse;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -12,6 +22,8 @@ import org.springframework.transaction.annotation.Transactional;
 public class StoryService {
 
     private final DiaryRepository diaryRepository;
+    private final MemberRepository memberRepository;
+    private final HeartRepository heartRepository;
 
     @Transactional
     public void createStory(Long diaryId, Long memberId) {
@@ -35,5 +47,50 @@ public class StoryService {
         }
 
         targetDiary.unShareToStory();
+    }
+
+    public StoryFindResponse findStory(Long diaryId, Long memberId) {
+        Member currentMember = memberRepository.findById(memberId)
+            .orElseThrow(RuntimeException::new);
+
+        Diary foundStory = diaryRepository.findStoryWithDetails(diaryId)
+            .orElseThrow(RuntimeException::new);
+
+        List<String> images = foundStory.getDiaryImages().stream()
+            .map(DiaryImage::getUrl)
+            .toList();
+
+        Optional<Heart> heartOptional = heartRepository.findByMemberAndDiary(currentMember,
+            foundStory);
+
+        List<CommentFindResponse> comments = new ArrayList<>();
+
+        if(foundStory.getComments() != null) {
+            comments = foundStory.getComments().stream()
+                .map(comment -> {
+                    Member commentAuthor = comment.getMember();
+
+                    return CommentFindResponse.builder()
+                        .commentId(comment.getId())
+                        .commentAuthorImage(commentAuthor.getProfileImage().getUrl())
+                        .commentAuthor(commentAuthor.getName())
+                        .comment(comment.getContent())
+                        .build();
+                })
+                .toList();
+        }
+
+        Member author = foundStory.getMember();
+
+        return StoryFindResponse.builder()
+            .authorImage(author.getProfileImage().getUrl())
+            .authorGroup(author.getGroup().getName())
+            .images(images)
+            .content(foundStory.getContent())
+            .createdDate(foundStory.getCreatedAt())
+            .favoriteState(heartOptional.isPresent())
+            .favoriteCount(foundStory.getHeartCount())
+            .comments(comments)
+            .build();
     }
 }
