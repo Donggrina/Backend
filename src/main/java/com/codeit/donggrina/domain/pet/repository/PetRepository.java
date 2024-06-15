6d799481465d58f2ba80279382c347405bd2ebd8
문현준 <jann1653@gmail.com>
2024-06-15 20:08:18 +0900

    fix: PetFindResponse 생성자 수정으로 인해 생성자 쿼리 수정

--- src/main/java/com/codeit/donggrina/domain/pet/repository/PetRepository.java
+++ src/main/java/com/codeit/donggrina/domain/pet/repository/PetRepository.java
@@ -9,7 +9,7 @@ import org.springframework.data.jpa.repository.Query;
 public interface PetRepository extends JpaRepository<Pet, Long> {
 
     @Query("select new com.codeit.donggrina.domain.pet.dto.response.PetFindResponse(" +
-        "pi.url, p.name, p.sex, p.birthDate, p.adoptionDate, " +
+        "pi.id, pi.url, p.name, p.sex, p.birthDate, p.adoptionDate, " +
         "p.type, p.species, p.weight, p.isNeutered) " +
         "from Pet p " +
         "join p.profileImage pi " +
