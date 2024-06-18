7610130a73722571b338ad2e50c223cb446f158c
문현준 <jann1653@gmail.com>
2024-06-18 11:12:05 +0900

    fix: 성장기록(GrowthHistory) 조회 관련해서 사용자가 그룹에 속해 있지 않은지 검증하는 예외 처리 과정 추가

--- src/main/java/com/codeit/donggrina/domain/growth_history/service/GrowthHistoryService.java
+++ src/main/java/com/codeit/donggrina/domain/growth_history/service/GrowthHistoryService.java
@@ -42,16 +42,22 @@ public class GrowthHistoryService {
     }
 
     public GrowthHistoryDetailResponse getDetail(Long growthId, Long memberId) {
-        Member member = memberRepository.findById(memberId)
+        Member member = memberRepository.findByIdWithGroup(memberId)
             .orElseThrow(() -> new IllegalArgumentException("존재하지 않는 사용자입니다."));
+        Optional.ofNullable(member.getGroup())
+            .orElseThrow(() -> new IllegalArgumentException("그룹에 속해 있지 않은 사용자입니다."));
+
         GrowthHistory growthHistory = growthHistoryRepository.findGrowthHistoryDetail(growthId);
         boolean isMine = growthHistory.getMember().equals(member);
         return GrowthHistoryDetailResponse.from(growthHistory, isMine);
     }
 
     public List<GrowthHistoryListResponse> search(SearchFilter searchFilter, Long memberId) {
-        Member member = memberRepository.findById(memberId)
+        Member member = memberRepository.findByIdWithGroup(memberId)
             .orElseThrow(() -> new IllegalArgumentException("존재하지 않는 사용자입니다."));
+        Optional.ofNullable(member.getGroup())
+            .orElseThrow(() -> new IllegalArgumentException("그룹에 속해 있지 않은 사용자입니다."));
+
         return growthHistoryRepository.findGrowthHistoryBySearchFilter(searchFilter)
             .stream()
             .map(growthHistory -> {
