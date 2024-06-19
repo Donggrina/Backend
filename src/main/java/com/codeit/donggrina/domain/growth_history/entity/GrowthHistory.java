fc151df624e6bd1415872c8ea4cc7e7e3c27bebc
문현준 <jann1653@gmail.com>
2024-06-19 17:04:07 +0900

    fix: 외래키에 on delete cascade 옵션 추가

--- src/main/java/com/codeit/donggrina/domain/growth_history/entity/GrowthHistory.java
+++ src/main/java/com/codeit/donggrina/domain/growth_history/entity/GrowthHistory.java
@@ -5,25 +5,23 @@ import com.codeit.donggrina.domain.growth_history.dto.request.GrowthHistoryUpdat
 import com.codeit.donggrina.domain.growth_history.util.CategoryEnumConverter;
 import com.codeit.donggrina.domain.member.entity.Member;
 import com.codeit.donggrina.domain.pet.entity.Pet;
-import com.codeit.donggrina.domain.pet.entity.SexValueConverter;
 import jakarta.persistence.Column;
 import jakarta.persistence.Convert;
 import jakarta.persistence.Entity;
-import jakarta.persistence.EnumType;
-import jakarta.persistence.Enumerated;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.ForeignKey;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
-import jakarta.persistence.Lob;
 import jakarta.persistence.ManyToOne;
 import java.time.LocalDate;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
 
 @Entity
 @Getter
@@ -35,10 +33,11 @@ public class GrowthHistory extends Timestamp {
     private Long id;
 
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "member_id", nullable = false, foreignKey = @ForeignKey(name = "fk_member_growth_history" ))
+    @JoinColumn(name = "member_id", nullable = false, foreignKey = @ForeignKey(name = "fk_member_growth_history"))
     private Member member;
 
     @ManyToOne(fetch = FetchType.LAZY)
+    @OnDelete(action = OnDeleteAction.CASCADE)
     @JoinColumn(name = "pet_id", nullable = false, foreignKey = @ForeignKey(name = "fk_pet_growth_history"))
     private Pet pet;
 
