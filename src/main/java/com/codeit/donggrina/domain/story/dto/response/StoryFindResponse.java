fa54ae2bc4646c8dab67f09cc2d50ad182fa2b4f
kwonMInwoo <qwerty029369@naver.com>
2024-06-20 14:13:52 +0900

    fix: 스토리 조회 수정
    - 데이터 추가

--- src/main/java/com/codeit/donggrina/domain/story/dto/response/StoryFindResponse.java
+++ src/main/java/com/codeit/donggrina/domain/story/dto/response/StoryFindResponse.java
@@ -1,6 +1,7 @@
 package com.codeit.donggrina.domain.story.dto.response;
 
 import com.codeit.donggrina.domain.comment.dto.response.CommentFindResponse;
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.List;
 import lombok.Builder;
@@ -8,10 +9,13 @@ import lombok.Builder;
 @Builder
 public record StoryFindResponse(
     String authorImage,
+    String author,
+    List<String> petImages,
+    String weather,
     String authorGroup,
     List<String> images,
     String content,
-    LocalDateTime createdDate,
+    LocalDate date,
     boolean favoriteState,
     int favoriteCount,
     List<CommentFindResponse> comments
